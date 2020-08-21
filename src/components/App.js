e8d361f558b97f6fbe3e4b73bc752e7f608791c7
Madeline Uribes <madeline.uribes@gmail.com>
2020-08-21 14:09:54 -0400

    Added featured mix and audio player

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div >
-        <div>
+        <div className='flex-l justify-end'>
           {/* FeaturedMix */}
           <FeaturedMix />
           <div>
@@ -20,7 +20,8 @@ class App extends Component {
         <iframe
           width="100%"
           height="60"
-          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fopen-hand-real-flames-22nd-january-2020%2F" frameBorder="0" >
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fopen-hand-real-flames-22nd-january-2020%2F" frameBorder="0"
+          className="db fixed bottom-0 z-5">
         </iframe>
       </div>
     );
