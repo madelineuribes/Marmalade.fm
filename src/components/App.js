be3b4e93dd378bd7560cc75420597e9361a07c9a
Madeline Uribes <madeline.uribes@gmail.com>
2020-08-23 18:46:46 -0400

    Mixclouds audio widget library

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
+/* global MixCloud */
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import FeaturedMix from './FeaturedMix';
 import Header from './Header';
@@ -15,6 +11,19 @@ const Archive = () => <h1>Archive</h1>
 const About = () => <h1>About</h1>
 
 class App extends Component {
+  mountAudio = async () => {
+    const widget = MixCloud.PlayerWidget(this.player);
+    await widget.ready
+    await widget.play()
+    console.log(widget);
+  };
+
+  componentDidMount() {
+    // when app component is loaded to page, our component gets called 
+    // so we can run mountAudio
+    this.mountAudio()
+  }
+
   render() {
     return (
       <Router>
@@ -36,7 +45,8 @@ class App extends Component {
             width="100%"
             height="60"
             src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fopen-hand-real-flames-22nd-january-2020%2F" frameBorder="0"
-            className="db fixed bottom-0 z-5">
+            className="db fixed bottom-0 z-5"
+            ref={player => (this.player = player)}>
           </iframe>
         </div>
       </Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 const Header = props => (
   <header className='black mb5 pt5'>
     <h1 className='ttu f3 tracked-mega anton tc mt0 mb3'>Marmalade.fm</h1>
     <ul className='list flex justify-center p10'>
       <li className='mh2'>
-        <NavLink to='/' className="nav-link link biryani-black f6 ttu gray">
+        <NavLink exact to='/' className='nav-link link biryani-black f6 ttu gray'>
           What's hot
         </NavLink>
       </li>
