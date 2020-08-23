a3204114f7cd4c9ecf16899cb8384c43881b36ca
Madeline Uribes <madeline.uribes@gmail.com>
2020-08-23 18:18:49 -0400

    Using React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import FeaturedMix from './FeaturedMix';
 import Header from './Header';
 
+
+const Home = () => <h1>Home</h1>
+const Archive = () => <h1>Archive</h1>
+const About = () => <h1>About</h1>
+
 class App extends Component {
   render() {
     return (
-      <div >
-        <div className='flex-l justify-end'>
-          {/* FeaturedMix */}
-          <FeaturedMix />
-          <div>
-            {/* Header */}
-            <Header />
-            {/* Routed Page */}
+      <Router>
+        <div >
+          <div className='flex-l justify-end'>
+            {/* FeaturedMix */}
+            <FeaturedMix />
+            <div className='w-50-l relative z-1'>
+              {/* Header */}
+              <Header />
+              {/* Routed Page */}
+              <Route exact path="/" component={Home} />
+              <Route path="/archive" component={Archive} />
+              <Route path="/about" component={About} />
+            </div>
           </div>
+          {/* AutioPlayer */}
+          <iframe
+            width="100%"
+            height="60"
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fopen-hand-real-flames-22nd-january-2020%2F" frameBorder="0"
+            className="db fixed bottom-0 z-5">
+          </iframe>
         </div>
-        {/* AutioPlayer */}
-        <iframe
-          width="100%"
-          height="60"
-          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fopen-hand-real-flames-22nd-january-2020%2F" frameBorder="0"
-          className="db fixed bottom-0 z-5">
-        </iframe>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NavLink } from "react-router-dom";
 
 const Header = props => (
   <header className='black mb5 pt5'>
     <h1 className='ttu f3 tracked-mega anton tc mt0 mb3'>Marmalade.fm</h1>
     <ul className='list flex justify-center p10'>
       <li className='mh2'>
-        <a className='nav-link link biryani-black f6 ttu gray'>
-          Popular
-        </a>
+        <NavLink to='/' className="nav-link link biryani-black f6 ttu gray">
+          What's hot
+        </NavLink>
       </li>
-      <li className='mh2'>        <a className='nav-link link black biryani-black f6 ttu gray'>
-        Archive
-        </a></li>
-      <li className='mh2'>        <a className='nav-link link black biryani-black f6 ttu gray'>
-        About
-        </a></li>
+      <li className='mh2'>
+        <NavLink to='/archive' className='nav-link link black biryani-black f6 ttu gray'>
+          Archive
+        </NavLink>
+      </li>
+      <li className='mh2'>
+        <NavLink to='/about' className='nav-link link black biryani-black f6 ttu gray'>
+          About
+        </NavLink>
+      </li>
     </ul>
   </header>
 );
